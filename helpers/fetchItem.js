211af774c78b842973a0186f1eded7bc4412b053
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-07-27 15:38:24 -0300

    feat: ensure fetchItem returns product data on success

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (itemId) => {
+  try {
+    const url = `https://api.mercadolibre.com/items/${itemId}`;
+    const response = await fetch(url);
+    const product = await response.json();
+    return product;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
